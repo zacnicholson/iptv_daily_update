a48c26aa6760b7ec24b39d51ae5c091dc87cd38f
zacnicholson <24601142+zacnicholson@users.noreply.github.com>
2022-09-08 14:00:34 +0700

    cleaned up save locations

--- free_iptv.py
+++ free_iptv.py
@@ -1,3 +1,4 @@
+from genericpath import isfile
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
@@ -5,13 +6,15 @@ from time import sleep
 import random
 import csv
 import threading
-import pyperclip
-
+import os
+from webdriver_manager.chrome import ChromeDriverManager
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument('window-size=1920x1080')
 chrome_options.add_argument("--disable-gpu")
+prefs = {'download.default_directory' : 'iptv_daily'}
+chrome_options.add_experimental_option('prefs', prefs)
 
 def who():
     global company, email, zipcode, first, last, job, phone, street_address, macos, state, ios
@@ -51,18 +54,24 @@ def who():
     phone = random.randint(1111111111,9999999999)
     print(phone)
 
-    job = ["Ceo", "COO", "Head of Security", "Business Manager", "Security Officer", "CFO", "Admin", "HR Manager", "IT Manager", "Director of IT", "Director of Security", "Information Security Analyst", "Network Security Administrator", "Cyber Crime Investigator", "Network Security Engineer", "Security Manager"]
-    job = random.choice(job)
-    print(job)
+    # job = ["Ceo", "COO", "Head of Security", "Business Manager", "Security Officer", "CFO", "Admin", "HR Manager", "IT Manager", "Director of IT", "Director of Security", "Information Security Analyst", "Network Security Administrator", "Cyber Crime Investigator", "Network Security Engineer", "Security Manager"]
+    # job = random.choice(job)
+    # print(job)
 
     street_address_number = str(random.randint(1,2000))
     street_address = street_address_number + " " + company
-    ios = random.randint(10, 2000)
+    # ios = random.randint(10, 2000)
 
-class kandji_spam:
+class free_iptv_auto:
+    if os.path.isfile("iptv_daily/iptv_daily_update.m3u"):
+        os.remove("iptv_daily/iptv_daily_update.m3u")
+    else:
+        pass
  
     def __init__(self):
-        self.driver = webdriver.Chrome('/Users/zacnicholson/Documents/GitHub/kandji spam/chromedriver', options=chrome_options) #, options=chrome_options
+        self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+
+        # self.driver = webdriver.Chrome('chromedriver', options=chrome_options) #, options=chrome_options
 
     def free_trial(self):
             ### Order free IPTV
@@ -73,7 +82,7 @@ class kandji_spam:
             sleep(5)
             check_out = self.driver.find_element_by_xpath('/html/body/center/font/b/center/center/center/center/center/center/section[3]/div/div/div[1]/div/div/div[2]/div[3]/div[2]/div/div[2]/div[5]/a[1]')
             check_out.click()
-
+            print('checkout complete')
 
 
             ### Fill out into 
@@ -99,7 +108,9 @@ class kandji_spam:
             password_confirm.send_keys('mypa!&ssword')
             approve_terms = self.driver.find_element_by_xpath('/html/body/center/font/b/center/center/center/center/center/center/section[3]/div/div/div[1]/div/div/div[2]/form/div[13]/p/label/div/ins').click()
             complete_order = self.driver.find_element_by_xpath('/html/body/center/font/b/center/center/center/center/center/center/section[3]/div/div/div[1]/div/div/div[2]/form/div[13]/button').click()
+            print('sign up complete')
 
+            ###Downloading the m3u
             self.driver.get('https://best-usa-hosting.com/clientarea.php?action=services')
             sleep(3)
             my_iptv = self.driver.find_element_by_xpath('/html/body/center/font/b/center/center/center/center/center/center/section[3]/div/div/div[3]/div/div[1]/div[1]/table/tbody/tr[1]')
@@ -107,17 +118,31 @@ class kandji_spam:
             sleep(2)
             iptv_details = self.driver.find_element_by_xpath('/html/body/center/font/b/center/center/center/center/center/center/section[3]/div/div/div[3]/div/div[1]/div/div[5]/div[1]/form/button')
             iptv_details.click()
-            # iptv_link = self.driver.find_element_by_xpath('/html/body/center/font/b/center/center/center/center/center/center/section[3]/div/div/div[3]/div/div[1]/div[3]/div[2]/div[2]/button')
-            # iptv_link.click()
             iptv_link = self.driver.find_element_by_xpath('/html/body/center/font/b/center/center/center/center/center/center/section[3]/div/div/div[3]/div/div[1]/center/a')
             save_link = iptv_link.get_attribute('href')
-            with open('iptv_daily_update', "w") as o:
-                o.write(save_link)
+            self.driver.get(save_link)
+            print(f'downloaded started. file name is {save_link}')
+            sleep(90)
+            folder = r'iptv_daily/'
+            count = 1
+            # count increase by 1 in each iteration
+            # iterate all files from a directory
+            for file_name in os.listdir(folder):
+                # Construct old file name
+                source = folder + file_name
 
+                # Adding the count to the new file name and extension
+                destination = folder + "iptv_daily_update.m3u"
 
+                # Renaming the file
+                os.rename(source, destination)
+                count += 1
 
+            print('New Names are')
+            # verify the result
+            res = os.listdir(folder)
+            print(res)
             print("finished")
-            sleep(6)
 
             
 
@@ -128,7 +153,7 @@ class kandji_spam:
 
 def do_requests():
         who()
-        bot = kandji_spam()
+        bot = free_iptv_auto()
         bot.free_trial()
 
 do_requests()
